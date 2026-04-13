146a7e644c8457ebbaf9823bacefb5e4f2bd8cad
João Guilherme Aguiar Moreira <jgam3010@gmail.com>
2026-04-13 12:35:25 -0300

    feat: week 06 project checkpoint

---
+++ project/script/cases.js
@@ -0,0 +1,38 @@
+export const causes = [
+  {
+    id: 1,
+    title: "Help João",
+    short_description: "João needs urgent medical treatment.",
+    goal: 10000,
+    raised: 3500,
+    image: "images/john.png",
+    category: "Health"
+  },
+  {
+    id: 2,
+    title: "Food for Dona Rosa",
+    short_description: "Fundraising to buy basic food supplies.",
+    goal: 2500,
+    raised: 1300,
+    image: "images/rose.png",
+    category: "Food"
+  },
+  {
+    id: 3,
+    title: "Carlos Surgery",
+    short_description: "Urgent surgery to improve quality of life.",
+    goal: 20000,
+    raised: 8000,
+    image: "images/carlos.png",
+    category: "Health"
+  },
+  {
+    id: 4,
+    title: "Support Hope Shelter",
+    short_description: "Help maintain a children's shelter.",
+    goal: 15000,
+    raised: 7000,
+    image: "images/hope-shelter.png",
+    category: "Organization"
+  }
+];
